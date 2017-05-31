3127417bfdf9149d9da96a6355765007da4860d7
Nick Latmos <nlatmos@gmail.com>
2017-05-31 21:48:00 +0300

    Update settings.py

--- django_website/settings.py
+++ django_website/settings.py
@@ -29,37 +29,6 @@ TIME_ZONE = 'Europe/Athens'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-LOGGING = {
-    'version': 1,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    }
-}
-
-if DEBUG:
-    # make all loggers use the console.
-    for logger in LOGGING['loggers']:
-        LOGGING['loggers'][logger]['handlers'] = ['console']
 	
 ALLOWED_HOSTS = [
 	'192.168.1.60',
