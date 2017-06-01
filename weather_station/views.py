400988956cb3ce1e84db7fcb9cbd981563a3e4b2
Nick Latmos <nlatmos@gmail.com>
2017-06-01 11:47:14 +0300

    Update views.py

--- weather_station/views.py
+++ weather_station/views.py
@@ -30,17 +30,16 @@ class Weather(APIView):
       raise Http404
 
   def post(self, request, format=None):
+    logger.warn("request body: %s", request.body)
     serializer = WeatherSerializer(data=request.data)
     # If the posted weather station's ID is not equal to anyone from the database then reject it.
     if serializer.is_valid():
       serializer.save()
       logger.warn('Successfully posted data:\n %s', json.dumps(request.POST, indent=4, sort_keys=True))
       self.pretty_request(request, logger)
-      logger.warn("request body: %s", request.body)
       return (Response('{Data successfully posted}', status=status.HTTP_201_CREATED))
     self.pretty_request(request, logger)
     logger.warn('Here is the post data:\n %s', json.dumps(request.POST, indent=4, sort_keys=True))
-    logger.warn("request body: %s", request.body)
     return (Response('{Something went wrong}', status=status.HTTP_400_BAD_REQUEST))
   
   def pretty_request(self, request, logger):
