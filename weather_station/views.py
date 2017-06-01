dd04f00a1bd06481d13b88770b8286a907514d30
Nick Latmos <nlatmos@gmail.com>
2017-06-01 11:10:11 +0300

    Update views.py

--- weather_station/views.py
+++ weather_station/views.py
@@ -35,10 +35,34 @@ class Weather(APIView):
     if serializer.is_valid():
       serializer.save()
       logger.warn('Successfully ppsted data:\n %s', json.dumps(request.POST, indent=4, sort_keys=True))
+      pretty_request(request, logger)
       return (Response('{Data successfully posted}', status=status.HTTP_201_CREATED))
     logger.warn("Let's see the fault")
     logger.warn('Here is the post data:\n %s', json.dumps(request.POST, indent=4, sort_keys=True))
+    pretty_request(request, logger)
     return (Response('{Something went wrong}', status=status.HTTP_400_BAD_REQUEST))
+  
+  def pretty_request(request, logger):
+    headers = ''
+    for header, value in request.META.items():
+        if not header.startswith('HTTP'):
+            continue
+        header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
+        headers += '{}: {}\n'.format(header, value)
+
+    logger.warn(
+        '{method} HTTP/1.1\n'
+        'Content-Length: {content_length}\n'
+        'Content-Type: {content_type}\n'
+        '{headers}\n\n'
+        '{body}'
+    ).format(
+        method=request.method,
+        content_length=request.META['CONTENT_LENGTH'],
+        content_type=request.META['CONTENT_TYPE'],
+        headers=headers,
+        body=request.body,
+    )
 
 class WeatherToday(APIView):
   '''
