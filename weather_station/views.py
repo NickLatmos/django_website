678d283289150ea56bdb5eb86477bccd1a0f5263
Nick Latmos <nlatmos@gmail.com>
2017-06-05 21:52:01 +0300

    Update views.py

--- weather_station/views.py
+++ weather_station/views.py
@@ -133,9 +133,9 @@ class WeatherDateTimeRange(APIView):
     dateObjectTo = datetime.datetime(year=int(year_to), month=int(month_to), day=int(day_to))
     timeObjectTo = datetime.time(hour=int(hour_to), minute=int(minute_to), second=int(second_to))
     weather = WeatherModel.objects.filter(ID=weather_station_id)
-    weather = weather.filter(date__gte=dateObjectFrom)
-    weather = weather.filter(time__gte=timeObjectFrom)
-    weather = weather.filter(date__lte=dateObjectTo)
+    #weather = weather.filter(date__gte=dateObjectFrom)
+    #weather = weather.filter(time__gte=timeObjectFrom)
+    #weather = weather.filter(date__lte=dateObjectTo)
     weather = weather.filter(time__lte=timeObjectTo)
     if not weather:
       raise Http404
