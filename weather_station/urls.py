661f4187298b9781260bd555f1213705b33decaa
Nick Latmos <nlatmos@gmail.com>
2017-06-13 12:13:21 +0300

    Create urls.py

--- weather_station/urls.py
+++ weather_station/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
   # url: /weather_station/XYZ/month/04
   url(r'^(?P<weather_station_id>[\w]+)/last_measurements/(?P<max_number>[0-9]{1,3})/interval/(?P<interval>[0-9]{1,3})/$', views.WeatherLastMeasurements.as_view()),
   # url: /weather_station/XYZ/2017/04/06
-  url(r'^(?P<weather_station_id>[\w]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.WeatherSpecificDate.as_view()),
+  url(r'^(?P<weather_station_id>[\w]+)/battery_voltage/$', views.WeatherStationVoltage.as_view()),
   # url: /weather_station/XYZ/2017/04/06
   url(r'^(?P<weather_station_id>[\w]+)/fromdate/(?P<year_from>[0-9]{4})/(?P<month_from>[0-9]{2})/(?P<day_from>[0-9]{2})/todate/(?P<year_to>[0-9]{4})/(?P<month_to>[0-9]{2})/(?P<day_to>[0-9]{2})/fromtime/(?P<hour_from>[0-9]{2})/(?P<minute_from>[0-9]{2})/(?P<second_from>[0-9]{2})/totime/(?P<hour_to>[0-9]{2})/(?P<minute_to>[0-9]{2})/(?P<second_to>[0-9]{2})/$', views.WeatherDateTimeRange.as_view()),
 ]
